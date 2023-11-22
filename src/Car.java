56800e16a66f38e481d123afc92a6474202ec17c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-22 15:29:07 +0300

    Car: Constructor is set

--- src/Car.java
+++ src/Car.java
@@ -3,4 +3,11 @@ public class Car {
     private int cylinder;
     private String name;
     private int wheels;
+
+    public Car(int cylinder, String name) {
+        this.cylinder = cylinder;
+        this.name = name;
+        engine = true;
+        wheels = 4;
+    }
 }
